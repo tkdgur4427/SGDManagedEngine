84667e737d72d514fb3f9b2ca288e83590826d13
Brian Hong <tkdgur4427@outlook.com>
2017-07-24 19:05:00 +0900

    patch

---
+++ SGDManagedEngine/SGDManagedEngine/Renderer/Renewal/SGDManagedRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGDManagedEngine.Renderer.Renewal
+{
+    // renewal for managed renderer to make it more platform-independent
+    public class H1ManagedRenderer
+    {
+
+    }
+}
